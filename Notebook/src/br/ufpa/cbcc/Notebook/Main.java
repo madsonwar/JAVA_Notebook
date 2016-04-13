bb10a063df08a047eef69357433458201c3a80d7
madsonwar <madsonwar21@gmail.com>
2016-04-13 16:17:26 -0300

    Create Main.java

---
+++ Notebook/src/br/ufpa/cbcc/Notebook/Main.java
@@ -0,0 +1,16 @@
+package br.ufpa.cbcc.Notebook;
+public class Main{
+public static void main(String[] args){
+  ComputadorPessoal computadorpessoal = new ComputadorPessoal();
+  ComputadorPessoal Hp = new ComputadorPessoal("Hp", "Avast", false, 100,"Mozila");
+  
+  computadorpessoal.ligarDesligar();
+  hp.getMarca();
+  computadorpessoal.aumentarVolume(50);
+  computadorpessoal.getVolume();
+  
+  
+  
+
+  }
+}
